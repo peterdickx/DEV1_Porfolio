f949432fcdf2b42e2dec2a2c016e0424e0cce98a
peter dickx <peter@DT-NOTE-DICKXP.ehb.local>
2019-10-01 16:52:10 +0200

    up to excercise 2

--- excercises/1-functions/1-1-lines.js
+++ excercises/1-functions/1-1-lines.js
@@ -1,2 +1,15 @@
 'use strict';
 import context from "../../context/context.js";
+
+function drawFish() {
+
+    context.lineWidth = 5;
+    context.beginPath();
+    context.moveTo(57, 390);
+    context.lineTo(390, 95);
+    context.lineTo(550, 320);
+    context.stroke();
+
+}
+
+drawFish();
