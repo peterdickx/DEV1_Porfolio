a1771450c4f46bde2c44819043a4d2991b38c572
Peter Dickx <peter.dickx@ehb.be>
2019-10-02 14:07:41 +0200

    fixed startcode

--- excercises/1-functions/1-1-lines.js
+++ excercises/1-functions/1-1-lines.js
@@ -1,15 +1,2 @@
 'use strict';
 import context from "../../context/context.js";
-
-function drawFish() {
-
-    context.lineWidth = 5;
-    context.beginPath();
-    context.moveTo(57, 390);
-    context.lineTo(390, 95);
-    context.lineTo(550, 320);
-    context.stroke();
-
-}
-
-drawFish();
